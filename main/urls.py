161c034d01e8353130a1aa2f7567aa0449a111e4
eelizabethmichelle <michellelizabethah@gmail.com>
2023-09-18 00:28:58 +0700

    Modify url for create_item

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-product', create_item, name='create_item'),
+    path('create-item', create_item, name='create_item'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
